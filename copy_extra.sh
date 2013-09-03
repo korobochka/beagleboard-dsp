37de498c0f004e555ac76b854ede48527b5576af
Korobochka <ilya-korobitsyn@narod.ru>
2013-09-03 14:52:30 +0400

    Fix copy script to include hidden files

--- copy_extra.sh
+++ copy_extra.sh
@@ -9,7 +9,7 @@ then
 	exit 1;
 fi
 
-cp -r ./extra/root/*  "$NFS_DIR"/home/root/
+cp -r ./extra/root/  "$NFS_DIR"/home/
 cp ./extra/faad/libgstfaad.so "$NFS_DIR"/usr/lib/gstreamer-0.10/
 cp ./extra/faad/libfaad.so.2.0.0 "$NFS_DIR"/usr/lib/
 cp ./extra/faad/libfaad.so.2 "$NFS_DIR"/usr/lib/
